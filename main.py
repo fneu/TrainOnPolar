7852fb6c3067ca241c7ce8720f94b9d0ef31b75a
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-05-19 18:20:03 +0200

    feat: convert garmin json to flow json format

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import configparser
 import logging
 
 
-from trainonpolar import trainasone
+from trainonpolar import trainasone, polarflow
 
 # read login details and default data from config.ini
 config = configparser.ConfigParser()
@@ -14,5 +14,9 @@ logger = logging.getLogger("trainonpolar")
 logger.setLevel(config["DEFAULT"]["log_level"])
 
 tao_session = trainasone.login(config)
+flow_session = polarflow.login(config)
+
 next_run_url, next_run_date = trainasone.next_run(tao_session)
 workout = trainasone.get_workout(tao_session, next_run_url)
+
+phased_target = polarflow.phased_target_from_garmin(workout, next_run_date)
---
+++ trainonpolar/polarflow.py
@@ -0,0 +1,74 @@
+import datetime
+import logging
+
+import requests_html
+
+from trainonpolar.formatting import pace, duration
+
+logger = logging.getLogger(__name__)
+
+
+def login(config):
+    session = requests_html.HTMLSession()
+    login_data = {
+        'email': config["polarflow"]["email"],
+        'password': config["polarflow"]["password"],
+    }
+    r = session.post(
+        'https://flow.polar.com/login',
+        data=login_data
+    )
+    if not r.url.startswith('https://flow.polar.com/diary'):
+        logger.error("Cannot connect to Polar Flow: Login failed.")
+        exit(1)
+    logger.debug("Login to Polar Flow succeeded.")
+    return session
+
+
+def phased_target_from_garmin(workout: dict, date: datetime.datetime):
+    target = {
+        "type": "PHASED",
+        "description": "",
+        "datetime": date.strftime("%Y-%m-%dT%H:%M"),
+        "exerciseTargets": [{
+            "id": None,
+            "distance": None,
+            "calories": None,
+            "duration": None,
+            "index": 0,
+            "sportId": 1,  # running
+            "phases": [phase_from_garmin(step) for step in workout["steps"]]
+        }]
+    }
+    logger.debug(f"Converted workout to polar flow json format: {target}")
+    return target
+
+
+def phase_from_garmin(step: dict):
+    if step["type"] == "WorkoutRepeatStep":
+        return {
+            "phaseType": "REPEAT",
+            "repeatCount": int(step["repeatValue"]),
+            "phases": [phase_from_garmin(s) for s in step["steps"]]
+        }
+    else:
+        return {
+            "id": None,
+            "lowerZone": 1,
+            "upperZone": 5,
+            "intensityType": "SPEED_ZONES",
+            "phaseChangeType": "AUTOMATIC",
+            "goalType": ("DISTANCE"
+                         if step["durationType"] == "DISTANCE"
+                         else "DURATION"),
+            "duration": ("00:00:00"
+                         if step["durationType"] == "DISTANCE"
+                         else duration(step["durationValue"])),
+            "distance": (int(step["durationValue"])
+                         if step["durationType"] == "DISTANCE"
+                         else None),
+            "name": f"{pace(step['targetValueLow'])}__"
+                    f"{pace(step['targetValue'])}__"
+                    f"{pace(step['targetValueHigh'])}",
+            "phaseType": "PHASE"
+        }
