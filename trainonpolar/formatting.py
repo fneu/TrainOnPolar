c20c4dcec5cbc918f7eeaf84804847414b9dd999
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-05-19 22:27:41 +0200

    fix: actually fix pace calculation

--- trainonpolar/formatting.py
+++ trainonpolar/formatting.py
@@ -1,11 +1,11 @@
 def pace(decimal):
     """Converts meters per second to a string in the format of M:SS"""
-    return f"{int(1000 / decimal // 60)}:{int(1000 / decimal % 60):02d}"
+    return f"{int(1000 / decimal // 60)}:{int(1000 / decimal % 60 + 0.5):02d}"
 
 
 def duration(seconds):
     """Converts seconds to a string in the format of HH:MM:SS"""
-    return f"{int(seconds // 3600):02d}:{int(seconds % 3600 // 60):02d}:{int(seconds % 60 + 0.5):02d}"
+    return f"{int(seconds // 3600):02d}:{int(seconds % 3600 // 60):02d}:{int(seconds % 60):02d}"
 
 
 def word_filter(text):
