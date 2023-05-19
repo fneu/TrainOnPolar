be948f75d01f8b3cc8599f2a95eac32b9b4d0fe1
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-05-19 22:06:11 +0200

    fix: correctly round pace seconds

--- trainonpolar/formatting.py
+++ trainonpolar/formatting.py
@@ -1,8 +1,8 @@
 def pace(decimal):
-    """Converts meters per second to a string in the format of MM:SS"""
-    return f"{int(1000 / decimal // 60):02d}:{int(1000 / decimal % 60):02d}"
+    """Converts meters per second to a string in the format of M:SS"""
+    return f"{int(1000 / decimal // 60)}:{int(1000 / decimal % 60):02d}"
 
 
 def duration(seconds):
     """Converts seconds to a string in the format of HH:MM:SS"""
-    return f"{int(seconds // 3600):02d}:{int(seconds % 3600 // 60):02d}:{int(seconds % 60):02d}"
+    return f"{int(seconds // 3600):02d}:{int(seconds % 3600 // 60):02d}:{int(seconds % 60 + 0.5):02d}"
